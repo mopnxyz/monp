774457a87db00ec54262fbaa8fb94afe5712a6c0
MOPN <121006356+mopnxyz@users.noreply.github.com>
2023-01-07 00:50:45 +0800

    Update app.1b8fa4fd.js

--- assets/app.1b8fa4fd.js
+++ assets/app.1b8fa4fd.js
@@ -1,12 +1,5 @@
-    self.addEventListener('activate', (e) => {
-      e.waitUntil(
-        caches.keys().then((t) => {
-          return Promise.all(
-            t.map((n) => {
-              return caches.delete(n)
-            })
-          )
-        })
-      )
-    })
-    self.skipWaiting()
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach(sw => sw.unregister())
+  })
+}

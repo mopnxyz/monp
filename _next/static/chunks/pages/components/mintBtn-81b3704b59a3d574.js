ab36797dbc010949e807c8672c9a2cfcf5652693
shikai <121006356+mopnxyz@users.noreply.github.com>
2023-03-24 23:34:43 +0800

    fix style

---
+++ _next/static/chunks/pages/components/mintBtn-81b3704b59a3d574.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[913],{95667:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/mintBtn",function(){return u(83954)}])}},function(n){n.O(0,[8312,3954,9774,2888,179],function(){return n(n.s=95667)}),_N_E=n.O()}]);
